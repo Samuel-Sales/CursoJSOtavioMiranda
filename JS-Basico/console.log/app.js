e94544743f958bcd82051c916016e3b3aa879cee
Samuel-Sales <cacabrbf@hotmail.com>
2024-08-08 23:13:09 -0300

    Done

---
+++ JS-Basico/console.log/app.js
@@ -0,0 +1,12 @@
+// Serve para mostrar mensagens no console
+console.log('Samuel Sales');
+console.log("Samuel Sales");
+console.log(`Samuel Sales`);
+
+console.log("Samuel 'Sales'");
+console.log('Samuel "Sales"');
+console.log(`"Samuel" 'Sales'`);
+
+console.log(123456); // Number
+console.log(35, 15.85); // Ponto flutuante
+console.log(12, 87.67, 'Samuel Sales');
