242bfcd4215849fca5f4adad2cbcda612b9e56bd
Samuel-Sales <cacabrbf@hotmail.com>
2024-08-12 20:59:40 -0300

    Done

---
+++ JS-Basico/comentarios/comentarios.js
@@ -0,0 +1,8 @@
+// E escrever um comentario
+console.log('Ola mundo!'); // Aqui tem outro comentario
+
+console.log('linha 1');
+console.log('linha 2');
+console.log('linha 3');
+console.log('linha 4');
+// Vai ser ignorado
---
+++ JS-Basico/variaveis-com-let/app.js
@@ -0,0 +1,37 @@
+let nome = 'Joao'; // String
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo.');
+
+let nome1; // Declarou a variavel
+nome1 = 'Samuel'; // Inicializando a variavel
+console.log(nome1);
+nome1 = 'Wesley';
+console.log(nome1);
+
+// Regras
+// Nao podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+let nomeCliente = 'Samuel';
+console.log(nomeCliente);
+
+// Nao pode comecar o nome de uma variavel com um numero
+// let 1nome;
+
+// Nao pode conter espacos ou tracos
+// Utilizamos camelCase
+let nomeCompleto = 'Samuel Sales';
+console.log(nomeCompleto)
+
+// Case-Sensitive
+let nomeCliente1 = 'Samuel';
+let nomecliente = 'Sales';
+
+console.log(nomeCliente1, nomecliente);
+
+// Nao podemos redeclarar variaveis com let
+// Nao UTILIZE VAR, UTILIZE LET
+
